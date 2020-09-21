8199080509ddc948d8a0decf40d0ffaf27cdff6d
ATizik <alexander.tizik@data4life.com>
2020-09-21 16:55:54 +0200

    xcode project

---
+++ secureprefs/library/keychainwrapper/keychainwrapper/keychainwrapper/keychainwrapper.h
@@ -0,0 +1,13 @@
+//
+//  keychainwrapper.h
+//  keychainwrapper
+//
+//  Created by Alexander Tizik on 03.07.20.
+//  Copyright © 2020 Data4life. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface keychainwrapper : NSObject
+
+@end
---
+++ secureprefs/library/keychainwrapper/keychainwrapper/keychainwrapper/keychainwrapper.m
@@ -0,0 +1,13 @@
+//
+//  keychainwrapper.m
+//  keychainwrapper
+//
+//  Created by Alexander Tizik on 03.07.20.
+//  Copyright © 2020 Data4life. All rights reserved.
+//
+
+#import "keychainwrapper.h"
+
+@implementation keychainwrapper
+
+@end
